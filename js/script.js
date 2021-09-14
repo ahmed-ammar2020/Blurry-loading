12a52b5fe32644e1df898c3e525ab2c314e36349
ahmed-ammar2020 <ahmed.ammar0134@gmail.com>
2021-09-14 19:50:28 +0200

    Blurry loading

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+const loadText = document.querySelector(".loading-text");
+const header = document.querySelector(".header");
+
+let load = 0;
+
+const blurring = () => {
+  load++;
+
+  if (load > 99) {
+    clearInterval(count);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  header.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+let count = setInterval(blurring, 20);
+
+//maps set of numbers to a second set of numbers -> used to map load increase to opacity to get fade out effect
+const scale = (number, inMin, inMax, outMin, outMax) =>
+  ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
